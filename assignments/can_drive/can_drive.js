e8f3109b614ab3927aa17d805043843ee98f3d25
sneha-gowda <snehagowda9899@gmail.com>
2021-11-27 21:57:52 +0530

    Assignment completed

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,13 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if (!hasDrivingLiscence) {
+        return "You cannot drive"
+    }
+    if (hasDrivingLiscence && !isTired && !isSober) {
+        return "You can drive"
+    }
+    return "You shouldn't drive"
+
 }
 
 module.exports = CanDrive;
