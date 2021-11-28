06c03ca015e239b0005c3902c6b1020e7e178914
sneha-gowda <snehagowda9899@gmail.com>
2021-11-28 10:01:08 +0530

    Assignment Completed

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,12 +1,12 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
     if (!hasDrivingLiscence) {
-        return "You cannot drive"
+        return "You cannot drive";
     }
     if (hasDrivingLiscence && !isTired && isSober) {
-        return "You can drive"
+        return "You can drive";
     }
-    return "You shouldn't drive"
+    return "You shouldn't drive";
 
 }
 
